ee589ad84e80e354bc09aad597fcea2e4e2a6d92
Ramin Barati <rekino@gmail.com>
2026-03-07 22:52:33 +0100

    add parser

---
+++ src/parser.ts
@@ -0,0 +1,148 @@
+import { CstParser } from "chevrotain"
+import {
+  allTokens,
+  ObjectTok,
+  MorphismTok,
+  SubobjectTok,
+  OfTok,
+  LCurly,
+  RCurly,
+  LParen,
+  RParen,
+  Colon,
+  Comma,
+  Arrow,
+  Identifier,
+  SingleString,
+} from "./lexer.js"
+
+export class SelectParser extends CstParser {
+  constructor() {
+    super(allTokens);
+
+    const $ = this;
+
+    $.RULE("specFile", () => {
+      $.MANY(() => {
+        $.SUBRULE($.declaration)
+      })
+    })
+
+    $.RULE("declaration", () => {
+      $.OR([
+        { ALT: () => $.SUBRULE($.objectDecl) },
+        { ALT: () => $.SUBRULE($.morphismDecl) },
+        { ALT: () => $.SUBRULE($.subobjectDecl) }
+      ])
+    })
+
+    $.RULE("objectDecl", () => {
+      $.CONSUME(ObjectTok)
+      $.CONSUME(Identifier)
+
+      $.CONSUME(LCurly)
+
+      $.MANY(() => {
+        $.SUBRULE($.fieldDecl)
+      })
+
+      $.CONSUME(RCurly)
+    })
+
+    $.RULE("fieldDecl", () => {
+      $.CONSUME(Identifier)
+      $.CONSUME(Colon)
+      $.SUBRULE($.typeExpr)
+    })
+
+    $.RULE("paramList", () => {
+      $.SUBRULE($.param)
+
+      $.MANY(() => {
+        $.CONSUME(Comma)
+        $.SUBRULE2($.param)
+      })
+    })
+
+    $.RULE("param", () => {
+      $.CONSUME(Identifier)
+      $.CONSUME(Colon)
+      $.SUBRULE($.typeExpr)
+    })
+
+    $.RULE("morphismDecl", () => {
+      $.CONSUME(MorphismTok)
+
+      $.CONSUME(Identifier)
+
+      $.CONSUME(LParen)
+
+      $.OPTION(() => {
+        $.SUBRULE($.paramList)
+      })
+
+      $.CONSUME(RParen)
+
+      $.CONSUME(Arrow)
+
+      $.SUBRULE($.typeExpr)
+
+      $.SUBRULE($.block)
+    })
+
+    $.RULE("block", () => {
+      $.CONSUME(LCurly)
+
+      $.MANY(() => {
+        $.SUBRULE($.statement)
+      })
+
+      $.CONSUME(RCurly)
+    })
+
+    $.RULE("statement", () => {
+      $.CONSUME(SingleString)
+    })
+
+    $.RULE("subobjectDecl", () => {
+      $.CONSUME(SubobjectTok)
+
+      $.CONSUME(Identifier)
+
+      $.CONSUME(OfTok)
+
+      $.CONSUME2(Identifier)
+
+      $.SUBRULE($.predicateBlock)
+    })
+
+    $.RULE("predicateBlock", () => {
+      $.CONSUME(LCurly)
+
+      $.MANY(() => {
+        $.SUBRULE($.predicateStatement)
+      })
+
+      $.CONSUME(RCurly)
+    })
+
+    $.RULE("predicateStatement", () => {
+      $.CONSUME(StringLiteral)
+    })
+
+    $.RULE("typeExpr", () => {
+      $.SUBRULE($.atomicType)
+
+      $.OPTION(() => {
+        $.CONSUME(Arrow)
+        $.SUBRULE2($.typeExpr)
+      })
+    })
+
+    $.RULE("atomicType", () => {
+      $.CONSUME(Identifier)
+    })
+
+    this.performSelfAnalysis();
+  }
+}
